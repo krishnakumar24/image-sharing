891142bb23a4b42d505ff9e698e116494634b8a0
Krishna Kumar <akkg21@gmail.com>
2024-11-29 13:10:12 +0530

    add image form changes

--- src/pages/AddImage.alvug.js
+++ src/pages/AddImage.alvug.js
@@ -5,6 +5,8 @@ import { currentMember } from "wix-members-frontend";
 
 const loaderVideoSelector = "#loaderVideo";
 
+let uploadedFiles;
+
 $w.onReady(function () {
   try {
     $w(loaderVideoSelector).hide();
@@ -12,7 +14,19 @@ $w.onReady(function () {
     $w("#fileNeedErrorMsg").hide();
     $w("#max4MsgNormal").show();
     $w("#max4MsgError").hide();
-    $w("#imageUpload").onChange(() => $w("#fileNeedErrorMsg").hide()); // Used to remove the message once image is uploaded
+    $w("#imageUpload").onChange(async (event) => {
+      console.log("upload event ", event);
+      $w("#fileNeedErrorMsg").hide();
+      try {
+        if ($w("#imageUpload").value.length > 0) {
+          uploadedFiles = await $w("#imageUpload").uploadFiles();
+        } else {
+          uploadedFiles = [];
+        }
+      } catch (uploadError) {
+        console.error(uploadError);
+      }
+    });
 
     $w("#tagsInput").onCustomValidation((value) => {
       console.log("tagsInput value", value);
@@ -73,52 +87,42 @@ $w.onReady(function () {
       }
       $w(loaderVideoSelector).show();
 
-      $w("#imageUpload")
-        .uploadFiles()
-        .then((uploadedFiles) => {
-          const title = $w("#addImgTitleInput").value; // Assuming titleInput is the ID of the title input field
-          const image = uploadedFiles[0].fileUrl; // Assuming imageUpload is the ID of the image upload button
-          // const tags = $w("#tagsInput").value.split(","); // Assuming tags are comma-separated in the input field
-          const description = $w("#descriptionInput").value;
-          const isPublic = $w("#isPublicInput").value; // Assuming isPublicToggle is a toggle or dropdown for public status
+      const title = $w("#addImgTitleInput").value;
+      const image = uploadedFiles[0].fileUrl; // Assuming imageUpload is the ID of the image upload button
+      const description = $w("#descriptionInput").value;
+      const isPublic = $w("#isPublicInput").value;
 
-          //CMS Record Object
-          const newItem = {
-            title: title,
-            image: image,
-            description: description,
-            isPublic: isPublic,
-            arrayString: newTags,
-            userId: user._id,
-          };
+      //CMS Record Object
+      const newItem = {
+        title: title,
+        image: image,
+        description: description,
+        isPublic: isPublic,
+        arraystring: newTags,
+        userId: user._id,
+      };
 
-          console.log("newItem -- before inserting data: ", newItem);
+      console.log("newItem -- before inserting data: ", newItem);
 
-          // Insert the item into the collection
-          wixData
-            .insert("Images", newItem)
-            .then((result) => {
-              console.log("Item added:", result);
+      // Insert the item into the collection
+      wixData
+        .insert("Images", newItem)
+        .then((result) => {
+          console.log("Item added:", result);
 
-              Images_afterInsert(user._id, isPublic);
+          Images_afterInsert(user._id, isPublic);
 
-              $w(loaderVideoSelector).hide();
-              if (isPublic.toLowerCase() === "public") {
-                wixLocationFrontend.to("/");
-              } else {
-                wixLocationFrontend.to("/my-images");
-              }
-              // Add any success messages or redirects here
-            })
-            .catch((error) => {
-              console.error("Error adding item:", error);
-              // Handle errors here
-            });
+          $w(loaderVideoSelector).hide();
+          if (isPublic.toLowerCase() === "public") {
+            wixLocationFrontend.to("/");
+          } else {
+            wixLocationFrontend.to("/my-images");
+          }
+          // Add any success messages or redirects here
         })
-        .catch((uploadError) => {
-          //   $w("#text1").text = "File upload error";
-          console.log("File upload error: " + uploadError.errorCode);
-          console.log(uploadError.errorDescription);
+        .catch((error) => {
+          console.error("Error adding item:", error);
+          // Handle errors here
         });
     });
   } catch (e) {
