220c1ea0f394fada07a2e71c45063060a4a58896
Krishna Kumar <akkg21@gmail.com>
2025-01-11 09:38:09 +0530

    CRON job setup done with jobs.config

---
+++ src/backend/spi/automation-spi-config.js
@@ -0,0 +1,6 @@
+import * as automationsVeloActionProvider from "interfaces-automations-v1-velo-action-provider";
+
+/** @returns {import('interfaces-automations-v1-velo-action-provider').VeloActionConfig} */
+export function getConfig() {
+  return {};
+}
